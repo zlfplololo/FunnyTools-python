af6a43461690c6509a814c696f8fa1c55ad0961f
zlfplololo <157253047+zlfplololo@users.noreply.github.com>
2024-02-21 14:01:44 +0200

    FunnyTools v.2.0
    v.2.0

--- FunnyTools.py
+++ FunnyTools.py
@@ -7,16 +7,51 @@ def ev(num):
 def without(what, without):
 	RetArr = []
 	Ret = 
-	for i in range(0,1,len(what)):
-		RetArr.append(what[i])
+	for i in what:
+		RetArr.append(i)
 
-	if without in what:
+	if without in range(0,1,len(what)):
 		for i in range(0,1,len(RetArr)):
 			if RetArr[i] == without:
 				del RetArr[i]
 				i = 0
 
-	for i in range(0,1,len(RetArr)):
-		Ret += RetArr[i]
+	for i in RetArr:
+		Ret += i
 
 	return(Ret)
+
+def arrobj():
+	class arrtoobj:
+		def __init__(): 
+
+		def ArrToObj(arr):
+			class retclass:
+				def __init__(self,):
+					for i in range(0,1,len(arr)):
+						name = arr[i][0]
+						val = arr[i][1]
+
+						self.name = val
+
+					del name
+					del val
+
+			return retclass
+
+		def ObjToArr(objname):
+			ret = []
+			name = list(objname.__dict__.keys())
+			value = []
+			for i in name:
+				value.append(getattr(objname, i))
+
+			for i in range(0,1,len(name)):
+				ret.append([name[i], value[i]])
+
+			return ret
+
+			
+
+	return arrtoobj	
+			
--- readme.txt
+++ readme.txt
@@ -1,2 +1,3 @@
-FunnyTools ver 1.0
+FunnyTools ver 2.0
 added "ev()" chek even is a num, "without()" return string without first argumend; in 1.0
+added "arrobj()" with methods "ArrToObj()" & "ObjToArr()"; in 2.0
